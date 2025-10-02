d8ea0a95d179c5acdadd18b477306e8d9859ae8f
fl4v1u5 <flaarivero96@gmail.com>
2025-10-02 20:26:39 -0300

    Modificacion estructural y agregado de boton modo-dark

---
+++ script/dark.js
@@ -0,0 +1,14 @@
+const boton = document.getElementById('tema');
+const html = document.documentElement;
+
+boton.addEventListener('click', function() {
+    if (html.getAttribute('data-bs-theme') === 'dark'){
+        html.setAttribute('data-bs-theme', 'light');
+        boton.setAttribute('bg', 'dark');
+
+    }else{
+        html.setAttribute('data-bs-theme', 'dark');
+        boton.setAttribute('bg', 'dark');
+
+    }
+})
